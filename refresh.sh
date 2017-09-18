34bb48f71c745033b4667cedda9b891356fc4d90
brianarnold789 <sdksupport@vantiv.com>
2017-09-18 11:32:14 -0400

    update refresh script

--- refresh.sh
+++ refresh.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # remove old generated classes and perform a Gradle build
-rm build/generated/com/litle/sdk/generate/*.java && gradle cleanEclipse eclipse
+rm -f build/generated/com/litle/sdk/generate/*.java && gradle cleanEclipse eclipse
 
