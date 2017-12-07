ea5ad039a4ec9de28f65c38540c6b8664509fe81
brianarnold789 <brian.arnold@vantiv.com>
2017-12-07 16:11:53 -0500

    add test for void transactions

---
+++ src/functionalTest/java/com/cnp/sdk/TestVoid.java
@@ -0,0 +1,30 @@
+package com.cnp.sdk;
+
+import com.cnp.sdk.generate.*;
+import com.cnp.sdk.generate.Void;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestVoid {
+
+    private static CnpOnline cnp;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        cnp = new CnpOnline();
+    }
+
+    @Test
+    public void simpleVoid() throws Exception {
+        Void voidTxn = new Void();
+        voidTxn.setCnpTxnId(102948757348543643L);
+        voidTxn.setReportGroup("Planets");
+        voidTxn.setId("new_ID");
+        voidTxn.setCustomerId("my_customer");
+
+        VoidResponse response = cnp.dovoid(voidTxn);
+        assertEquals("000",response.getResponse());
+    }
+}
