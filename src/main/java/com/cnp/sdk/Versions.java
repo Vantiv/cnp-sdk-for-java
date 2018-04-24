3668dfbc4e737ebe1ca50da2ffb287749b8e192e
swapnilhande <swapnil.hande@gmail.com>
2018-04-24 13:36:41 -0400

    Updated release version

--- src/main/java/com/cnp/sdk/Versions.java
+++ src/main/java/com/cnp/sdk/Versions.java
@@ -3,6 +3,6 @@ package com.cnp.sdk;
 public class Versions {
 
     public static final String XML_VERSION="12.1";
-    public static final String SDK_VERSION="Java;12.1.0";
+    public static final String SDK_VERSION="Java;12.1.1";
 
 }
