98241c69f3725924627898432409d3e7ad8776cb
fborges1 <fborges@wpi.edu>
2018-06-11 15:51:50 -0400

    Updated version number in gradle.properties and versions.java

--- src/main/java/com/cnp/sdk/Versions.java
+++ src/main/java/com/cnp/sdk/Versions.java
@@ -3,6 +3,6 @@ package com.cnp.sdk;
 public class Versions {
 
     public static final String XML_VERSION="12.3";
-    public static final String SDK_VERSION="Java;12.3.0";
+    public static final String SDK_VERSION="Java;12.3.1";
 
 }
