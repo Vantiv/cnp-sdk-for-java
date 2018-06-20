8426b6b73acc3d40cc13a2c57511132aceca1039
swapnilhande <swapnil.hande@gmail.com>
2018-06-20 16:55:24 -0400

    upgrade minor version

--- src/main/java/com/cnp/sdk/Versions.java
+++ src/main/java/com/cnp/sdk/Versions.java
@@ -3,6 +3,6 @@ package com.cnp.sdk;
 public class Versions {
 
     public static final String XML_VERSION="12.3";
-    public static final String SDK_VERSION="Java;12.3.1";
+    public static final String SDK_VERSION="Java;12.3.2";
 
 }
