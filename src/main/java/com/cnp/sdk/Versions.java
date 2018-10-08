92bb2ae09c9bf6d4bfb40b84f584a7c4cce693d1
davekartik24 <davekartik24@gmail.com>
2018-10-08 11:47:39 -0400

    Version updated to 12.5.0 and deleted the old version XSD's

--- src/main/java/com/cnp/sdk/Versions.java
+++ src/main/java/com/cnp/sdk/Versions.java
@@ -3,6 +3,6 @@ package com.cnp.sdk;
 public class Versions {
 
     public static final String XML_VERSION="12.5";
-    public static final String SDK_VERSION="Java;12.5.1";
+    public static final String SDK_VERSION="Java;12.5.0";
 
 }
