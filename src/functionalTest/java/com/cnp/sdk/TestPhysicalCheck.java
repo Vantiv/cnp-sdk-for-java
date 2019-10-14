eda6480d6de58386810afe080349ecf7893e0b13
zxc8027 <zxc8027@g.rit.edu>
2019-10-14 08:37:45 -0400

    Added physical check tests.

---
+++ src/functionalTest/java/com/cnp/sdk/TestPhysicalCheck.java
@@ -0,0 +1,70 @@
+package com.cnp.sdk;
+
+import com.cnp.sdk.generate.*;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestPhysicalCheck {
+    private static CnpOnline cnp;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        cnp = new CnpOnline();
+    }
+
+    @Test
+    public void testPhysicalCheckCredit() throws Exception{
+        PhysicalCheckCredit pccredit = new PhysicalCheckCredit();
+        pccredit.setReportGroup("physicalCheckCredit");
+        pccredit.setId("111");
+        pccredit.setFundingSubmerchantId("physicalCheckCredit");
+        pccredit.setFundsTransferId("1001");
+        pccredit.setAmount(500l);
+
+        PhysicalCheckCreditResponse response = cnp.physicalCheckCredit(pccredit);
+        assertEquals("Approved", response.getMessage());
+    }
+
+    @Test
+    public void testPhysicalCheckCreditWithFundingCustomerId() throws Exception{
+        PhysicalCheckCredit pccredit = new PhysicalCheckCredit();
+        pccredit.setReportGroup("physicalCheckCredit");
+        pccredit.setId("111");
+        pccredit.setFundingCustomerId("physicalCheckCredit");
+        pccredit.setFundsTransferId("1001");
+        pccredit.setAmount(500l);
+
+        PhysicalCheckCreditResponse response = cnp.physicalCheckCredit(pccredit);
+        assertEquals("Approved", response.getMessage());
+    }
+
+    //
+
+    @Test
+    public void testPhysicalCheckDebit() throws Exception{
+        PhysicalCheckDebit pcdebit = new PhysicalCheckDebit();
+        pcdebit.setReportGroup("physicalCheckDebit");
+        pcdebit.setId("111");
+        pcdebit.setFundingSubmerchantId("physicalCheckDebit");
+        pcdebit.setFundsTransferId("1001");
+        pcdebit.setAmount(500l);
+
+        PhysicalCheckDebitResponse response = cnp.physicalCheckDebit(pcdebit);
+        assertEquals("Approved", response.getMessage());
+    }
+
+    @Test
+    public void testPhysicalCheckDebitWithFundingCustomerId() throws Exception{
+        PhysicalCheckDebit pcdebit = new PhysicalCheckDebit();
+        pcdebit.setReportGroup("physicalCheckDebit");
+        pcdebit.setId("111");
+        pcdebit.setFundingCustomerId("physicalCheckDebit");
+        pcdebit.setFundsTransferId("1001");
+        pcdebit.setAmount(500l);
+
+        PhysicalCheckDebitResponse response = cnp.physicalCheckDebit(pcdebit);
+        assertEquals("Approved", response.getMessage());
+    }
+}
