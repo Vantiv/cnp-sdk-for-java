35fce69663d1f1bd3e0c1e9ba6c05305ee561971
zxc8027 <zxc8027@g.rit.edu>
2019-10-14 08:21:11 -0400

    Added no customer id and customer id too long tests.

--- src/functionalTest/java/com/cnp/sdk/TestPayoutOrg.java
+++ src/functionalTest/java/com/cnp/sdk/TestPayoutOrg.java
@@ -19,7 +19,7 @@ public class TestPayoutOrg {
 
     @Test
     public void TestPayoutOrgCredit() throws Exception {
-       PayoutOrgCredit pocredit = new  PayoutOrgCredit();
+       PayoutOrgCredit pocredit = new PayoutOrgCredit();
         pocredit.setReportGroup("payoutOrgCredit");
         pocredit.setId("111");
         pocredit.setFundingCustomerId("payoutOrgCredit");
@@ -30,12 +30,47 @@ public class TestPayoutOrg {
         assertEquals("Approved", response.getMessage());
     }
 
+    @Test(expected = CnpOnlineException.class)
+    public void TestPayoutOrgCreditNullFundingCustomerId() throws Exception {
+        PayoutOrgCredit pocredit = new PayoutOrgCredit();
+        pocredit.setReportGroup("payoutOrgCredit");
+        pocredit.setId("111");
+        pocredit.setFundsTransferId("1001");
+        pocredit.setAmount(500l);
+
+        cnp.payoutOrgCredit(pocredit);
+    }
+
     @Test
     public void TestPayoutOrgDebit() throws Exception {
         PayoutOrgDebit podebit = new PayoutOrgDebit();
         podebit.setReportGroup("payoutOrgDebit");
         podebit.setId("111");
         podebit.setFundingCustomerId("payoutOrgDebit");
+        podebit.setFundsTransferId("1001");
+        podebit.setAmount(500l);
+
+        PayoutOrgDebitResponse response = cnp.payoutOrgDebit(podebit);
+        assertEquals("Approved", response.getMessage());
+    }
+
+    @Test(expected = CnpOnlineException.class)
+    public void TestPayoutOrgDebitNullFundingCustomerId() throws Exception {
+        PayoutOrgDebit podebit = new PayoutOrgDebit();
+        podebit.setReportGroup("payoutOrgDebit");
+        podebit.setId("111");
+        podebit.setFundsTransferId("1001");
+        podebit.setAmount(500l);
+
+        cnp.payoutOrgDebit(podebit);
+    }
+
+    @Test(expected = CnpOnlineException.class)
+    public void TestPayoutOrgDebitFundingCustomerIdTooLong() throws Exception {
+        PayoutOrgDebit podebit = new PayoutOrgDebit();
+        podebit.setReportGroup("payoutOrgDebit");
+        podebit.setId("111");
+        podebit.setFundingCustomerId("012345678901234567890123456789012345678901234567890123456789");
         podebit.setFundsTransferId("1001");
         podebit.setAmount(500l);
 
