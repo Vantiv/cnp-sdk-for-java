d829ac87d18c55ba78f010a87e411ad0ed7ed3e5
zxc8027 <zxc8027@g.rit.edu>
2019-10-15 14:46:54 -0400

    Updated XSDs to 12.10

--- src/main/java/com/cnp/sdk/Versions.java
+++ src/main/java/com/cnp/sdk/Versions.java
@@ -2,7 +2,7 @@ package com.cnp.sdk;
 
 public class Versions {
 
-    public static final String XML_VERSION="12.9";
-    public static final String SDK_VERSION="Java;12.9";
+    public static final String XML_VERSION="12.10";
+    public static final String SDK_VERSION="Java;12.10";
 
 }
