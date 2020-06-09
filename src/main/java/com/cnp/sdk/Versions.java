1b2c56a08f6db23e2a5fc5c5e8f42205f3b0df2a
Liam Bewley <lsb3562@rit.edu>
2020-06-09 12:56:15 -0400

    Minor fix for versioning

--- src/main/java/com/cnp/sdk/Versions.java
+++ src/main/java/com/cnp/sdk/Versions.java
@@ -3,6 +3,6 @@ package com.cnp.sdk;
 public class Versions {
 
     public static final String XML_VERSION="12.13";
-    public static final String SDK_VERSION="Java;12.13";
+    public static final String SDK_VERSION="Java;12.13.0";
 
 }
