f38e878cc450798caf1edea6aed9987c5440b09c
nglod33 <nglod@nglod-vm1.dwi.litle.com>
2020-07-08 15:46:49 -0400

    Updated xsd's and versioning numbers

--- src/main/java/com/cnp/sdk/Versions.java
+++ src/main/java/com/cnp/sdk/Versions.java
@@ -2,7 +2,7 @@ package com.cnp.sdk;
 
 public class Versions {
 
-    public static final String XML_VERSION="12.13";
-    public static final String SDK_VERSION="Java;12.13.0";
+    public static final String XML_VERSION="12.14";
+    public static final String SDK_VERSION="Java;12.14.0";
 
 }
