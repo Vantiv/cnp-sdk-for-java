02dcd8a3e94708dd681ae3f3c0423355886ae446
GregoryConrad <gsconrad@wpi.edu>
2020-11-18 13:33:11 -0500

    Presumably finished the code for the new ResponseFileParser

--- src/main/java/com/cnp/sdk/ResponseFileParser2.java
+++ src/main/java/com/cnp/sdk/ResponseFileParser2.java
@@ -20,8 +20,9 @@ import com.cnp.sdk.generate.CnpTransactionInterface;
  * However, batch files can get extremely large, so for efficiency, it is best to parse them bit by bit with a buffer
  */
 public class ResponseFileParser2 {
-    private static String TRANSACTION_RESPONSE_REGEX = "<(\\w+Response)>.*?</\\1>";
-    private static String GENERATE_PACKAGE_NAME = "com.cnp.sdk.generate";
+    private static final String TRANSACTION_RESPONSE_REGEX = "<(\\w+Response)>.*?</\\1>";
+    private static final String GENERATE_PACKAGE_NAME = "com.cnp.sdk.generate";
+    private static final String BATCH_RESPONSE_FILE_ENCODING = "UTF-8";
 
     private Map<String, Matcher> tagMap = new HashMap<>();
     private Matcher transactionResponseMatcher;
@@ -29,6 +30,7 @@ public class ResponseFileParser2 {
 
     /**
      * Creates a response file parser based off of the given batch response file
+     *
      * @param responseFile the batch response file to parse
      * @throws IOException in case any file-handling issues occur
      */
@@ -36,7 +38,7 @@ public class ResponseFileParser2 {
         FileInputStream is = new FileInputStream(responseFile);
         FileChannel channel = is.getChannel();
         ByteBuffer byteBuffer = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size());
-        bufferedFile = Charset.forName("UTF-8").newDecoder().decode(byteBuffer);
+        bufferedFile = Charset.forName(BATCH_RESPONSE_FILE_ENCODING).newDecoder().decode(byteBuffer);
         channel.close();
         is.close();
         transactionResponseMatcher = Pattern.compile(TRANSACTION_RESPONSE_REGEX, Pattern.DOTALL).matcher(bufferedFile);
@@ -46,28 +48,37 @@ public class ResponseFileParser2 {
      * In short, this method gets the next tag of the specified name
      * If the tag is a cnpResponse or batchResponse, the insides of the tag are left out for performance reasons
      * Otherwise, the full tag and all of its data are returned as xml
+     *
      * @param tagToLookFor the tag to search for in the xml response
      * @return the xml of the next instance of tagToLookFor found
      */
     public String getNextTag(String tagToLookFor) {
+        // Remove the (inside) xml contents of tags known to have very large children for performance
+        boolean removeXmlContents = tagToLookFor.equals("cnpResponse") || tagToLookFor.equals("batchResponse");
         if (!tagMap.containsKey(tagToLookFor)) {
             // Regex that first tries to find <tagToLookFor ... /> but if it can't,
             //     resorts to searching for <tagToLookFor ...></tagToLookFor>
-            Pattern pattern = Pattern.compile("<(" + tagToLookFor + ")(([^<>]*?/>)|(.*?</\\1>))", Pattern.DOTALL);
+            String regex = "<(" + tagToLookFor + ")(([^<>]*?/>)|(.*?</\\1>))";
+            if (removeXmlContents) {
+                // Handle the special 2 cases where we don't want the insides of the xml for large responses
+                // This regex only matches the starting tag, but nothing inside or the closing tag
+                regex = "<" + tagToLookFor + ".*?>";
+            }
+            Pattern pattern = Pattern.compile(regex, Pattern.DOTALL);
             tagMap.put(tagToLookFor, pattern.matcher(bufferedFile));
         }
 
-        // todo cnpResponse and batchResponse should return just the tags (no insides), probs for performance
-
         Matcher matcher = tagMap.get(tagToLookFor);
         if (matcher.find()) {
-            return matcher.group();
+            // If the regex removed the contents & closing tag, add it back manually here, else return the entire match
+            return removeXmlContents ? matcher.group() + "</" + tagToLookFor + ">" : matcher.group();
         }
         throw new CnpBatchException("No more " + tagToLookFor + " tags left");
     }
 
     /**
      * Gets the next transaction response in the batch response file
+     *
      * @return the xml of the next transaction response in the batch response file
      */
     public String getNextTransactionResponse() {
@@ -85,7 +96,7 @@ public class ResponseFileParser2 {
                     return responseXml;
                 }
             }
-            catch (ClassNotFoundException ignored) {
+            catch (ClassNotFoundException ignored) { // no generated class for tag; ignore and continue to next match
             }
         }
         throw new CnpBatchException("No more transaction responses found");
