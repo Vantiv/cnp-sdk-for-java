70383b48a2241c9a6fcad1b103eb507f2bdf4fa4
Vinod Mishra <mishravinod202@gmail.com>
2021-02-04 08:04:30 -0500

    Adding changelog and sdk version information

--- src/main/java/com/cnp/sdk/Versions.java
+++ src/main/java/com/cnp/sdk/Versions.java
@@ -3,6 +3,6 @@ package com.cnp.sdk;
 public class Versions {
 
     public static final String XML_VERSION="12.19";
-    public static final String SDK_VERSION="Java;12.19.1";
+    public static final String SDK_VERSION="Java;12.19.0";
 
 }
