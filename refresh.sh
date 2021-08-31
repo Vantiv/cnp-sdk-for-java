5199994ff88ca3c5cc44dd047ec50c2ad22c230f
abhirud <aniket.bhirud@fisglobal.com>
2021-08-31 02:42:51 -0400

    Fixed Review Comments

--- refresh.sh
+++ refresh.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # remove old generated classes and perform a Gradle build
-rm -f build/generated/com/cnp/sdk/generate/*.java && gradle cleanEclipse eclipse
+rm -f build/generated/io/github/vantiv/sdk/generate/*.java && gradle cleanEclipse eclipse
 
