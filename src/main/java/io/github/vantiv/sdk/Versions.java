788738f3adbcd33c084fe5c9b07cf7abea4a50a4
sgite <Surekha.Gite@fisglobal.com>
2022-05-12 09:16:22 -0400

    Adding schemas for v12.25 Changing Version in Versions.java and gradle.properties

--- src/main/java/io/github/vantiv/sdk/Versions.java
+++ src/main/java/io/github/vantiv/sdk/Versions.java
@@ -2,7 +2,7 @@ package io.github.vantiv.sdk;
 
 public class Versions {
 
-    public static final String XML_VERSION="12.24";
-    public static final String SDK_VERSION="Java;12.24.0";
+    public static final String XML_VERSION="12.25";
+    public static final String SDK_VERSION="Java;12.25.0";
 
 }
