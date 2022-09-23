3bf4238c781941642c692745438faaefb9b44fdf
Immaniyelu Sunnapudi <Immaniyelu.Sunnapudi@fisglobal.com>
2022-09-23 15:37:34 +0530

    added new version of sdk -12.27

--- src/main/java/io/github/vantiv/sdk/Versions.java
+++ src/main/java/io/github/vantiv/sdk/Versions.java
@@ -2,7 +2,7 @@ package io.github.vantiv.sdk;
 
 public class Versions {
 
-    public static final String XML_VERSION="12.25";
-    public static final String SDK_VERSION="Java;12.25.0";
+    public static final String XML_VERSION="12.27";
+    public static final String SDK_VERSION="Java;12.27.0";
 
 }
