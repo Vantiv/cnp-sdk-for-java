dae0084d73be4a6869f0233fd833d69dc8282233
Immaniyelu Sunnapudi <Immaniyelu.Sunnapudi@fisglobal.com>
2022-09-27 11:47:04 +0530

    unit tests for auth & sale

--- src/functionalTest/java/io/github/vantiv/sdk/TestAuth.java
+++ src/functionalTest/java/io/github/vantiv/sdk/TestAuth.java
@@ -660,4 +660,65 @@ public class TestAuth {
 		assertEquals( "Approved",response.getMessage());
 		assertEquals("sandbox", response.getLocation());
 	}
+	@Test
+	public  void simpleAuthWithPassengerTransportData() throws Exception{
+		Authorization authorization = new Authorization();
+		authorization.setReportGroup("русский中文");
+		authorization.setOrderId("12344");
+		authorization.setAmount(106L);
+		authorization.setOrderSource(OrderSourceType.ECOMMERCE);
+		authorization.setId("id");
+		CardType card = new CardType();
+		card.setType(MethodOfPaymentTypeEnum.VI);
+		card.setNumber("4100000000000000");
+		card.setExpDate("1210");
+		authorization.setCard(card);
+		authorization.setPassengerTransportData(passengerTransportData());
+		AuthorizationResponse response = cnp.authorize(authorization);
+		assertEquals("русский中文",response.getReportGroup());
+		assertEquals("sandbox", response.getLocation());
+	}
+
+	private PassengerTransportData passengerTransportData(){
+		PassengerTransportData passengerTransportData = new PassengerTransportData();
+		passengerTransportData.setPassengerName("PassengerName");
+		passengerTransportData.setTicketNumber("9876543210");
+		passengerTransportData.setIssuingCarrier("str4");
+		passengerTransportData.setCarrierName("CarrierName");
+		passengerTransportData.setRestrictedTicketIndicator("RestrictedIndicator");
+		passengerTransportData.setNumberOfAdults(8);
+		passengerTransportData.setNumberOfChildren(1);
+		passengerTransportData.setCustomerCode("CustomerCode");
+		passengerTransportData.setArrivalDate(Calendar.getInstance());
+		passengerTransportData.setIssueDate(Calendar.getInstance());
+		passengerTransportData.setTravelAgencyCode("TravCode");
+		passengerTransportData.setTravelAgencyName("TravelAgencyName");
+		passengerTransportData.setComputerizedReservationSystem(ComputerizedReservationSystemEnum.DATS);
+		passengerTransportData.setCreditReasonIndicator(CreditReasonIndicatorEnum.C);
+		passengerTransportData.setTicketChangeIndicator(TicketChangeIndicatorEnum.C);
+		passengerTransportData.setTicketIssuerAddress("IssuerAddress");
+		passengerTransportData.setExchangeAmount(new Long(110));
+		passengerTransportData.setExchangeFeeAmount(new Long(112));
+		passengerTransportData.setExchangeTicketNumber("ExchangeNumber");
+		passengerTransportData.getTripLegDatas().add(addTripLegData());
+		return  passengerTransportData;
+	}
+
+	private TripLegData addTripLegData(){
+		TripLegData tripLegData = new TripLegData();
+		tripLegData.setTripLegNumber(new BigInteger("4"));
+		tripLegData.setDepartureCode("DeptC");
+		tripLegData.setCarrierCode("CC");
+		tripLegData.setServiceClass(ServiceClassEnum.BUSINESS);
+		tripLegData.setStopOverCode("N");
+		tripLegData.setDestinationCode("DestC");
+		tripLegData.setFareBasisCode("FareBasisCode");
+		tripLegData.setDepartureDate(Calendar.getInstance());
+		tripLegData.setOriginCity("OCity");
+		tripLegData.setTravelNumber("TravN");
+		tripLegData.setDepartureTime("DepartureTime");
+		tripLegData.setArrivalTime("ArrivalTime");
+		tripLegData.setRemarks("Remarks");
+		return  tripLegData;
+	}
 }
