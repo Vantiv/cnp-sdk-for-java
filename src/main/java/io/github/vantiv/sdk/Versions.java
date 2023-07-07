de59729cb1209364aa2a19c5b162c0e12434c843
e5647349 <surekhagite32@gmail.com>
2023-07-07 13:00:03 +0530

    version related changes wrt latest version.

--- src/main/java/io/github/vantiv/sdk/Versions.java
+++ src/main/java/io/github/vantiv/sdk/Versions.java
@@ -2,7 +2,6 @@ package io.github.vantiv.sdk;
 
 public class Versions {
 
-    public static final String XML_VERSION="12.30";
-    public static final String SDK_VERSION="Java;12.30.0";
-
+    public static final String XML_VERSION="12.31";
+    public static final String SDK_VERSION="Java;12.31.0";
 }
