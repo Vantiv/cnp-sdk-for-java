a72454ad739683ed0c939dbb7eb90cfe70e06d45
sghadage <sushamaghadage38@gmail.com>
2024-05-06 09:00:13 -0400

    commit version 12.34.1 related changes.

--- src/main/java/io/github/vantiv/sdk/Versions.java
+++ src/main/java/io/github/vantiv/sdk/Versions.java
@@ -2,6 +2,6 @@ package io.github.vantiv.sdk;
 
 public class Versions {
 
-    public static final String XML_VERSION="12.34";
-    public static final String SDK_VERSION="Java;12.34.0";
+    public static final String XML_VERSION="12.34.1";
+    public static final String SDK_VERSION="Java;12.34.1";
 }
