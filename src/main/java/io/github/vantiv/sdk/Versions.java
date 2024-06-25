d0ae58f4bae63050511eed076c0ac78b36b3d647
Nitinsl <nitinlingayat.sh@gmail.com>
2024-06-25 13:12:59 +0530

    commiting version related changes

--- src/main/java/io/github/vantiv/sdk/Versions.java
+++ src/main/java/io/github/vantiv/sdk/Versions.java
@@ -3,5 +3,5 @@ package io.github.vantiv.sdk;
 public class Versions {
 
     public static final String XML_VERSION="12.37";
-    public static final String SDK_VERSION="Java;12.37.0";
+    public static final String SDK_VERSION="Java;12.37.1";
 }
