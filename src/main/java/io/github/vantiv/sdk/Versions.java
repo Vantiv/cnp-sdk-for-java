3cf4b8c987f7c1e60a5d7e4785e3f59c587a182f
Vinod Mishra <mishravinod202@gmail.com>
2024-10-28 12:06:46 -0400

    Upgrade CNP API version to 12.42

--- src/main/java/io/github/vantiv/sdk/Versions.java
+++ src/main/java/io/github/vantiv/sdk/Versions.java
@@ -2,6 +2,6 @@ package io.github.vantiv.sdk;
 
 public class Versions {
 
-    public static final String XML_VERSION="12.40";
-    public static final String SDK_VERSION="Java;12.40.0";
+    public static final String XML_VERSION="12.42";
+    public static final String SDK_VERSION="Java;12.42.0";
 }
