81c3c1bb5d413b777b71546f4a14758a9b4310db
e5651806 <sushamaghadage38@gmail.com>
2025-05-22 13:04:24 +0530

    commiting version related changes for v12.45

--- src/main/java/io/github/vantiv/sdk/Versions.java
+++ src/main/java/io/github/vantiv/sdk/Versions.java
@@ -3,5 +3,5 @@ package io.github.vantiv.sdk;
 public class Versions {
 
     public static final String XML_VERSION = "12.45";
-    public static final String SDK_VERSION = "Java;12.45";
+    public static final String SDK_VERSION = "Java;12.45.0";
 }
