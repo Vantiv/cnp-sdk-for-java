7b9c1888c2454977c809c34d1626e8d68d18a9db
e5651806 <sushamaghadage38@gmail.com>
2025-05-23 17:10:52 +0530

    commiting CHANGELOG TypeofDigitalCurrencyEnum for v12.45.

---
+++ src/main/java/io/github/vantiv/sdk/TypeOfDigitalCurrencyEnum.java
@@ -0,0 +1,44 @@
+package io.github.vantiv.sdk;
+
+import io.github.vantiv.sdk.generate.ForeignRetailerIndicatorEnum;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType(name = "typeOfDigitalCurrencyEnum")
+@XmlEnum
+public enum TypeOfDigitalCurrencyEnum {
+
+    @XmlEnumValue("1")
+    ONE("1"),
+    @XmlEnumValue("2")
+    TWO("2"),
+    @XmlEnumValue("3")
+    THREE("3"),
+    @XmlEnumValue("4")
+    FOUR("4"),
+    @XmlEnumValue("7")
+    SEVEN("7");
+
+    private final String value;
+
+    TypeOfDigitalCurrencyEnum(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+
+}
+
+
+    public static TypeOfDigitalCurrencyEnum fromValue(String value) {
+        for (TypeOfDigitalCurrencyEnum enumValue : TypeOfDigitalCurrencyEnum.values()) {
+            if (enumValue.value.equals(value)) {
+                return enumValue;
+            }
+        }
+        throw new IllegalArgumentException("Invalid value: " + value);
+    }
+}
