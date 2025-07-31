958bb496f3cacfbe610852d8206a46951f1f4e9f
e5651806 <sushamaghadage38@gmail.com>
2025-07-31 14:52:08 +0530

    commiting changes for 12.46 and 12.47.

--- src/functionalTest/java/io/github/vantiv/sdk/TestAuth.java
+++ src/functionalTest/java/io/github/vantiv/sdk/TestAuth.java
@@ -346,6 +346,7 @@ public class TestAuth {
 	    DetailTax dt2 = new DetailTax();
 	    dt2.setTaxAmount(200L);
 	    enhanced.getDetailTaxes().add(dt2);
+        enhanced.setNumberOfPayments("2");
 	    authorization.setEnhancedData(enhanced);
 	    CardType card = new CardType();
 	    card.setNumber("4100000000000000");
@@ -516,11 +517,12 @@ public class TestAuth {
 		EnhancedData enhanced = new EnhancedData();
 		enhanced.setCustomerReference("Cust Ref");
 		enhanced.setSalesTax(1000L);
-		enhanced.setNumberOfPayments("12");
+		enhanced.setNumberOfPayments("1");
 		LineItemData lid = new LineItemData();
 		lid.setItemSequenceNumber(1);
 		lid.setItemDescription("Electronics");
 		lid.setProductCode("El01");
+		lid.setLineItemDetailIndicator("0");
 		lid.setItemCategory("Ele Appiances");
 		lid.setItemSubCategory("home appliaces");
 		lid.setProductId("1001");
--- src/functionalTest/java/io/github/vantiv/sdk/TestBatchFile.java
+++ src/functionalTest/java/io/github/vantiv/sdk/TestBatchFile.java
@@ -782,6 +782,7 @@ public class TestBatchFile {
         line_Item.setItemSequenceNumber(1);
         line_Item.setItemDescription("Electronics");
         line_Item.setProductCode("El01");
+        line_Item.setLineItemDetailIndicator("10");
         line_Item.setItemCategory("Ele Appiances");
         line_Item.setItemSubCategory("home appliaces");
         line_Item.setProductId("1001");
@@ -798,6 +799,7 @@ public class TestBatchFile {
         enhancedData.getLineItemDatas().add(line_Item);
         enhancedData.setDiscountCode("oneTimeDis");
         enhancedData.setDiscountPercent(BigInteger.valueOf(12));
+        enhancedData.setNumberOfPayments("5");
         enhancedData.setFulfilmentMethodType(FulfilmentMethodTypeEnum.COUNTER_PICKUP);
         authorization_mit.setEnhancedData(enhancedData);
         authorization_mit.setOrderChannel(OrderChannelEnum.MIT);
@@ -1135,7 +1137,9 @@ public class TestBatchFile {
         sub.setNextDeliveryDate(Calendar.getInstance());
         lid.setShipmentId("456");
         lid.setSubscription(sub);
+        lid.setLineItemDetailIndicator("0");
         enhanced.getLineItemDatas().add(lid);
+        enhanced.setNumberOfPayments("4");
         enhanced.setDiscountCode("oneTimeDis");
         enhanced.setDiscountPercent(BigInteger.valueOf(12));
         enhanced.setFulfilmentMethodType(FulfilmentMethodTypeEnum.STANDARD_SHIPPING);
