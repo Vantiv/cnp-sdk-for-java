2577063a8f1f6d988b9769fbb348579834b297fe
e5651806 <sushamaghadage38@gmail.com>
2025-07-31 20:24:14 +0530

    Revert "commiting changes for 12.46 and 12.47."
    This reverts commit 9b666879b1876986168a9eb0f3c84d011f3adbaf.

--- src/main/java/io/github/vantiv/sdk/Communication.java
+++ src/main/java/io/github/vantiv/sdk/Communication.java
@@ -118,8 +118,6 @@ public class Communication {
         RequestTarget reqTarget = CommManager.instance(configuration).findUrl();
         HttpPost post = new HttpPost(reqTarget.getUrl());
         post.setHeader("Content-Type", CONTENT_TYPE_TEXT_XML_UTF8);
-        post.setHeader("ECOM_API","");
-
 
         post.setConfig(requestConfig);
         HttpEntity entity = null;
